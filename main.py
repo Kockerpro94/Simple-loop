f11df93faa1473520b1a9ce7f5b8afbac8f9c966
Kockerpro <kockerpro94@gmail.com>
2025-08-11 21:43:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print("---------------------------------------------------------------")
+print("this is for educational purpose only")
+print("---------------------------------------------------------------")
+print("Maked by: Kockerpro94")
+print("---------------------------------------------------------------")
+
+print("what you want String to loop?")
+
+stringToLoop = input("Enter a string to loop: ")
+
+print("How many times you want to loop it?")
+
+infiNumber = input("Enter a number to make it For loop: ")
+
+for i in range(int(infiNumber)):
+    print(str(i) + " " + stringToLoop)
+    
